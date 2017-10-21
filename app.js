1d98f31db7915384dfb6ce52c7e79d2d32ddf984
DESKTOP-FHHFM48\Michael <michael9389@gmail.com>
2017-10-21 22:26:52 +0100

    added 'index' route

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongdb://localhost/restful_blog_app');
-app.set('view_engine', 'ejs');
+app.listen(3000, '127.0.0.1', function () {
+    console.log("server is running");
+});
+
+mongoose.connect('mongodb://localhost/restful_blog_app');
+app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -16,3 +20,25 @@ var blogSchema = new mongoose.Schema({
 });
 
 var Blog = mongoose.model("Blog", blogSchema);
+
+// Blog.create({
+//     title: "First Blog",
+//     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzDpvl2EH43LKPnGpc4BkZpRMFfNGeFkSv7Y_ryv8mTxbVMg8b5A",
+//     body: "This is the body text"
+// });
+
+//Routes
+
+app.get("/", function (req, res) {
+    res.redirect("/blogs");
+});
+
+app.get("/blogs", function (req, res) {
+    Blog.find({}, function (err, blogs) {
+        if(err){
+            console.log(err);
+        }else{
+            res.render("index", {blogs: blogs});
+        }
+    })
+});
