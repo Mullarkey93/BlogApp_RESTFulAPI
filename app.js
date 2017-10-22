99831c4f863ac0293d22b589d7fd3a02cd08024f
DESKTOP-FHHFM48\Michael <michael9389@gmail.com>
2017-10-22 14:49:20 +0100

    added the 'show' route, aswell as additional styling

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.get("/", function (req, res) {
     res.redirect("/blogs");
 });
 
+//Index Route
 app.get("/blogs", function (req, res) {
     Blog.find({}, function (err, blogs) {
         if(err){
@@ -42,3 +43,31 @@ app.get("/blogs", function (req, res) {
         }
     })
 });
+
+//New Route
+app.get("/blogs/new", function (req, res) {
+    res.render("new");
+});
+
+//Create Route
+app.post("/blogs", function (req, res) {
+
+    Blog.create(req.body.blog, function (err, newBlog) {
+        if(err){
+            res.render("new")
+        }else{
+            res.redirect("/blogs");
+        }
+    })
+});
+
+//Show Route
+app.get("/blogs/:id", function (req, res) {
+    Blog.findById(req.params.id, function (err, foundBlog) {
+        if(err){
+            res.redirect("/blogs")
+        }else{
+            res.render("show", {blog: foundBlog});
+        }
+    })
+})
